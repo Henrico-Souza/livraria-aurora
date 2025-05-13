cd9157a11c7b0fc5a63f5af76448c1cd405b12b2
Henrico Souza <henrico.projects@gmail.com>
2025-05-12 23:35:29 -0300

    feat: criar componentes que compõem a home page

---
+++ src/app/shared/components/slide-carousel/slide-carousel.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-slide-carousel',
+  standalone: true,
+  imports: [],
+  templateUrl: './slide-carousel.component.html',
+  styleUrl: './slide-carousel.component.scss'
+})
+export class SlideCarouselComponent {
+
+}
