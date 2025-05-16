3e549bed0f410bf6213a7e7b55737c9558481dfb
Henrico Souza <henrico.projects@gmail.com>
2025-05-15 22:53:56 -0300

    chore(navbar): importa módulos para searchbar

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,17 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatListModule } from '@angular/material/list';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators'
+
+
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatSidenavModule, MatIconModule, MatButtonModule, MatListModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, ReactiveFormsModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
-  isMenuOpen = false;
+
+export class NavbarComponent implements OnInit{
+  searchControl = new FormControl('');
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        console.log('Busca:', value);
+      });
+  }
+
 }
