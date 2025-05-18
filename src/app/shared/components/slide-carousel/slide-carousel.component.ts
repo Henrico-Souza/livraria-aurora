af1c4d950a5fbc9edd1c1d2d03bda19836f65a02
Henrico Souza <henrico.projects@gmail.com>
2025-05-18 17:23:26 -0300

    chore(slider): adiciona schema 'CUSTOM_ELEMENTS_SCHEMA' ao slide-carousel

--- src/app/shared/components/slide-carousel/slide-carousel.component.ts
+++ src/app/shared/components/slide-carousel/slide-carousel.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 
 @Component({
   selector: 'app-slide-carousel',
   standalone: true,
   imports: [],
   templateUrl: './slide-carousel.component.html',
-  styleUrl: './slide-carousel.component.scss'
+  styleUrl: './slide-carousel.component.scss',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
 })
 export class SlideCarouselComponent {
 
