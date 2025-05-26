8539e36b4acbc980a60b0131a39ea77b18ba5d60
Henrico Souza <henrico.projects@gmail.com>
2025-05-26 11:25:01 -0300

    style(navbar): adiciona estilização global da sidenav para visibilidade do dorpdown.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
-import { register } from 'swiper/element/bundle';
 
+import { register } from 'swiper/element/bundle';
 register();
 
 bootstrapApplication(AppComponent, appConfig)
