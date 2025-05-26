2feb2d680cddcbb5f58cd9a0f1bfd3bcbac56d82
Henrico Souza <henrico.projects@gmail.com>
2025-05-26 11:23:22 -0300

    chore(navbar): importa bibliotacas necessárias para o sidenav.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,43 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './core/components/navbar/navbar.component';
+import { SlideCarouselComponent } from './shared/components/slide-carousel/slide-carousel.component';
+
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from "@angular/material/list";
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormControl } from '@angular/forms';
+import { debounceTime } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent],
+  imports: [RouterOutlet, NavbarComponent, SlideCarouselComponent, MatToolbarModule, MatIconModule, MatButtonModule, 
+    ReactiveFormsModule, MatMenuModule, MatSidenavModule, MatListModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'livraria-aurora';
+
+  searchControl = new FormControl('');
+
+  //Dropdowns
+  isLivrosOpen = false;
+  isPapelariaOpen = false;
+  isJogosOpen = false;
+  isInformaticaOpen = false;
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(300))
+      .subscribe((value) => {
+        console.log('Busca:', value);
+      });
+  }
+
 }
