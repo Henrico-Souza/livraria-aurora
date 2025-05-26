64562e384de6fa0515c197d59e05cd57389560f0
Henrico Souza <henrico.projects@gmail.com>
2025-05-26 11:20:17 -0300

    chore(navbar): importa bibliotecas necessárias para os departamentos.

--- src/app/core/components/navbar/navbar.component.ts
+++ src/app/core/components/navbar/navbar.component.ts
@@ -1,17 +1,21 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, Input, OnInit} from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatListModule } from "@angular/material/list";
 import { ReactiveFormsModule } from '@angular/forms';
 import { FormControl } from '@angular/forms';
 import { debounceTime } from 'rxjs/operators'
+import { MatSidenav } from '@angular/material/sidenav';
 
 
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [MatToolbarModule, MatIconModule, MatButtonModule, ReactiveFormsModule],
+  imports: [MatToolbarModule, MatIconModule, MatButtonModule, 
+    ReactiveFormsModule, MatMenuModule, MatListModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
@@ -26,5 +30,7 @@ export class NavbarComponent implements OnInit{
         console.log('Busca:', value);
       });
   }
+
+  @Input() sidenav!: MatSidenav;
 
 }
